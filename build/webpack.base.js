77b9cb6d4d5d1ff7b78e85dcca2a66736f041401
fangchaoqun <fang.chaoqun@nissei-china.cn>
2021-12-28 16:30:32 +0800

    test: github actions

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -1,16 +1,16 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 // const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const ProgressBarPlugin = require("progress-bar-webpack-plugin");
-const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
-const WebpackBuildNotifierPlugin = require('webpack-build-notifier');
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
+const WebpackBuildNotifierPlugin = require("webpack-build-notifier");
 const chalk = require("chalk");
-const isDev = process.env.NODE_ENV === 'development' ? true : false;
+const isDev = process.env.NODE_ENV === "development" ? true : false;
 
-console.log("isDev===>", isDev)
+console.log("isDev===>", isDev);
 
 const rootDir = process.cwd();
 
@@ -43,30 +43,32 @@ module.exports = {
         test: /\.(le|c)ss$/,
         use: [
           // 开发模式下用style-loader
-          isDev ? 'style-loader' : MiniCssExtractPlugin.loader, 
+          isDev ? "style-loader" : MiniCssExtractPlugin.loader,
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             // 解决 css 命名混乱和冲突
-            options: isDev ? {
-              importLoaders: 2
-            } : {
-              // 意思是，如果在css中引入(@import)了其他文件css,而这个css文件中引入了less,将用less-loader处理
-              importLoaders: 2,
-              modules: {
-                compileType: "module",
-                localIdentName: "[path][name]__[local]--[hash:base64:5]",
-              },
-            },
-          }, 
-          'postcss-loader',
-          'less-loader',
+            options: isDev
+              ? {
+                  importLoaders: 2,
+                }
+              : {
+                  // 意思是，如果在css中引入(@import)了其他文件css,而这个css文件中引入了less,将用less-loader处理
+                  importLoaders: 2,
+                  modules: {
+                    compileType: "module",
+                    localIdentName: "[path][name]__[local]--[hash:base64:5]",
+                  },
+                },
+          },
+          "postcss-loader",
+          "less-loader",
         ],
         exclude: /node_modules/,
       },
       // https://webpack.docschina.org/guides/asset-modules/
       {
         test: /\.(png|jpg|gif|jpeg|webp|svg|eot|ttf|woff|woff2)$/,
-        type: 'asset',
+        type: "asset",
       },
     ],
   },
@@ -79,20 +81,26 @@ module.exports = {
       template: path.resolve(rootDir, "public/index.html"),
     }),
     new CopyWebpackPlugin({
-      patterns: [{
-        from: path.resolve(rootDir, 'public/static'), //要打包的静态资源目录地址
-        to: path.resolve(rootDir, 'dist/static')
-      }]
-    }),,
+      patterns: [
+        {
+          from: path.resolve(rootDir, "public/static"), //要打包的静态资源目录地址
+          to: path.resolve(rootDir, "dist/static"),
+        },
+      ],
+    }),
+    ,
     new ProgressBarPlugin({
-      format: '  build [:bar] ' + chalk.green.bold(':percent') + ' (:elapsed seconds)',
-      clear: false
+      format:
+        "  build [:bar] " +
+        chalk.green.bold(":percent") +
+        " (:elapsed seconds)",
+      clear: false,
     }), // 优化进度显示
     new FriendlyErrorsWebpackPlugin(),
     new WebpackBuildNotifierPlugin({
-      title: "My Project Webpack Build",
+      title: "My Project Webpack Build!!",
       // logo: path.resolve("./img/favicon.png"),
-      suppressSuccess: true
-    })
+      suppressSuccess: true,
+    }),
   ].filter(Boolean),
-}
+};
