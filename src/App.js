eef087fec235ece78e72b0dd697619ff66d9b292
fangchaoqun <fang.chaoqun@nissei-china.cn>
2021-12-28 16:13:40 +0800

    chore: github actions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<div>react</div>, document.getElementById('app'))
+ReactDOM.render(<div>react</div>, document.getElementById("app"));
